c6f6b02919b435cbb79ae3c5d2f563b2346fc81e
Veera Vilpponen <vilpponen.veera@myy.haaga-helia.fi>
2019-05-03 13:06:22 +0300

    improved search and style

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5%;
+  margin-bottom: 5%;
 }
-b-container {
-  width: 80%;
-}
-
 </style>
